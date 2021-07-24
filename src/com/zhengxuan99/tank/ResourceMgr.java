78bd75b014821e8b25b0c6021453886e7b20d231
RENOLEVES <zzhao260985652@gmail.com>
2021-07-23 23:37:44 -0400

    置入坦克以及子弹的图片

--- src/com/zhengxuan99/tank/ResourceMgr.java
+++ src/com/zhengxuan99/tank/ResourceMgr.java
@@ -7,7 +7,9 @@ import java.util.Objects;
 
 public class ResourceMgr {
     public static BufferedImage GoodTankL,GoodTankU,GoodTankD,GoodTankR;
-
+    public static BufferedImage BadTankU, BadTankD, BadTankL,BadTankR;
+    public static BufferedImage BulletU,BulletD,BulletL,BulletR;
+    public static BufferedImage[] explodes = new BufferedImage[16];
 
     static {
         try {
@@ -15,6 +17,16 @@ public class ResourceMgr {
             GoodTankD = ImageUtil.rotateImage(GoodTankU, 180);
             GoodTankL = ImageUtil.rotateImage(GoodTankU, -90);
             GoodTankR = ImageUtil.rotateImage(GoodTankU, 90);
+
+            BadTankU = ImageIO.read(Objects.requireNonNull(Tank.class.getClassLoader().getResourceAsStream("image/BadTank.jpg")));
+            BadTankD = ImageUtil.rotateImage(BadTankU, 180);
+            BadTankL = ImageUtil.rotateImage(BadTankU, -90);
+            BadTankR = ImageUtil.rotateImage(BadTankU, 90);
+
+            BulletU = ImageIO.read(Objects.requireNonNull(Tank.class.getClassLoader().getResourceAsStream("image/Bullet.jpg")));
+            BulletD = ImageUtil.rotateImage(BulletU, 180);
+            BulletL = ImageUtil.rotateImage(BulletU, -90);
+            BulletR = ImageUtil.rotateImage(BulletU, 90);
 
         } catch (IOException e) {
             e.printStackTrace();
--- src/test/ImageTest.java
+++ src/test/ImageTest.java
@@ -1,14 +1,15 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class ImageTest {
 
     @Test
