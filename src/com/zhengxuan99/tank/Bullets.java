a5522692863a1d4b5ffbbaca85a8d918ab7c96ca
RENOLEVES <zzhao260985652@gmail.com>
2021-07-24 22:46:54 -0400

    子弹换成图片

--- src/com/zhengxuan99/tank/Bullets.java
+++ src/com/zhengxuan99/tank/Bullets.java
@@ -4,15 +4,15 @@ import java.awt.*;
 
 public class Bullets {
     private static final int SPEED = 20;
-    private static final int WIDTH = 15 , HEIGHT = 15;
+    private static final int WIDTH = 15, HEIGHT = 15;
     TankFrame tf = null;
 
-    private  Dir dir;
-    private int x,y;
+    private Dir dir;
+    private int x, y;
 
     private boolean live = true;
 
-    public Bullets(int x, int y,Dir dir,TankFrame tf) {
+    public Bullets(int x, int y, Dir dir, TankFrame tf) {
         this.x = x;
         this.y = y;
         this.dir = dir;
@@ -20,14 +20,29 @@ public class Bullets {
     }
 
 
-    public void paint(Graphics g){
-        if(!live){
+    public void paint(Graphics g) {
+        if (!live) {
             tf.bullets.remove(this);
         }
 
         Color c = g.getColor();
         g.setColor(Color.red);
-        g.fillOval(x,y,WIDTH, HEIGHT);
+
+        switch (dir) {
+            case UP:
+                g.drawImage(ResourceMgr.BulletU, x, y, null);
+                break;
+            case DOWN:
+                g.drawImage(ResourceMgr.BulletD, x, y, null);
+                break;
+            case LEFT:
+                g.drawImage(ResourceMgr.BulletL, x, y, null);
+                break;
+            case RIGHT:
+                g.drawImage(ResourceMgr.BulletR, x, y, null);
+                break;
+        }
+
         g.setColor(c);
 
         move();
@@ -35,24 +50,24 @@ public class Bullets {
     }
 
     private void move() {
-            switch (dir) {
-                case LEFT:
-                    x -= SPEED;
-                    break;
-                case UP:
-                    y -= SPEED;
-                    break;
-                case RIGHT:
-                    x += SPEED;
-                    break;
-                case DOWN:
-                    y += SPEED;
-                    break;
-            }
+        switch (dir) {
+            case LEFT:
+                x -= SPEED;
+                break;
+            case UP:
+                y -= SPEED;
+                break;
+            case RIGHT:
+                x += SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+        }
 
-            if(x<0 || y<0 || x > TankFrame.GAME_WIDTH || y > TankFrame.GAME_HEIGHT){
-                live = false;
-            }
+        if (x < 0 || y < 0 || x > TankFrame.GAME_WIDTH || y > TankFrame.GAME_HEIGHT) {
+            live = false;
+        }
     }
 
 }
